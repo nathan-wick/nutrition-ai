4362f50a339b7b8b20d8817234613358ce438ff1
Nathan Wick <nathanwick2001@gmail.com>
2023-10-02 15:43:24 -0400

    Sign-In With Email

--- nutrition_ai/lib/screens/authenticated/account.dart
+++ nutrition_ai/lib/screens/authenticated/account.dart
@@ -1,4 +1,7 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+
+import '../../widgets/my_button.dart';
 
 class Account extends StatefulWidget {
   const Account({super.key});
@@ -8,15 +11,41 @@ class Account extends StatefulWidget {
 }
 
 class _AccountState extends State<Account> {
-  static const _placeholder = Text(
-    'Account Screen Placeholder',
-  );
+  final user = FirebaseAuth.instance.currentUser!;
+
+  void signOut() {
+    FirebaseAuth.instance.signOut();
+  }
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: _placeholder,
+    return Scaffold(
+      body: SafeArea(
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              const SizedBox(height: 80),
+              const Icon(
+                Icons.account_circle,
+                size: 100,
+              ),
+              const SizedBox(height: 40),
+              Text(
+                'Signed in as ${user.email}',
+                style: const TextStyle(
+                  color: Colors.grey,
+                  fontSize: 16,
+                ),
+              ),
+              const SizedBox(height: 20),
+              MyButton(
+                onTap: signOut,
+                message: 'Sign Out',
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
---
+++ nutrition_ai/lib/widgets/square_tile.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+class SquareTile extends StatelessWidget {
+  final String imagePath;
+  const SquareTile({
+    super.key,
+    required this.imagePath,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.all(20),
+      decoration: BoxDecoration(
+        border: Border.all(color: Colors.grey),
+        borderRadius: BorderRadius.circular(4),
+        color: Colors.white,
+      ),
+      child: Image.asset(
+        imagePath,
+        height: 40,
+      ),
+    );
+  }
+}
