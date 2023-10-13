8ec8bf47a76b5d78b659a437bbc206a757ddfec1
Nathan Wick <nathanwick2001@gmail.com>
2023-10-13 13:11:50 -0400

    Get and Set User Auth Info In Database (#58)

--- nutrition_ai/lib/pages/authenticated/profile.dart
+++ nutrition_ai/lib/pages/authenticated/profile.dart
@@ -1,8 +1,10 @@
-import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_auth/firebase_auth.dart' as firebase_auth;
 import 'package:flutter/material.dart';
 
+import '../../models/user.dart';
 import '../../pages/authenticated/settings.dart';
 import '../../widgets/button_input.dart';
+import '../../services/database.dart';
 
 class Profile extends StatefulWidget {
   const Profile({super.key});
@@ -12,10 +14,16 @@ class Profile extends StatefulWidget {
 }
 
 class _ProfileState extends State<Profile> {
-  final user = FirebaseAuth.instance.currentUser!;
+  late Future<User> user;
 
   void signOut() {
-    FirebaseAuth.instance.signOut();
+    firebase_auth.FirebaseAuth.instance.signOut();
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    user = DatabaseService().getUser();
   }
 
   @override
@@ -27,49 +35,69 @@ class _ProfileState extends State<Profile> {
       ),
       body: SafeArea(
         child: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.start,
-            children: [
-              const SizedBox(height: 40),
-              Image.network(
-                user.photoURL ?? '',
-                width: 100,
-                height: 100,
-                errorBuilder: (BuildContext context, Object exception, StackTrace? stackTrace) {
-                  return const Icon(
-                    Icons.account_circle,
-                    size: 100,
-                  );
-                },
-              ),
-              const SizedBox(height: 40),
-              Text(
-                user.email ?? '',
-                style: const TextStyle(
-                  color: Colors.grey,
-                  fontSize: 16,
-                ),
-              ),
-              const SizedBox(height: 20),
-              ButtonInput(
-                onTap: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(builder: (context) => const Settings()),
-                  );
-                },
-                icon: Icons.settings,
-                message: 'Settings',
-                theme: ButtonInputTheme.primary,
-              ),
-              const SizedBox(height: 20),
-              ButtonInput(
-                onTap: signOut,
-                icon: Icons.arrow_back,
-                message: 'Sign Out',
-                theme: ButtonInputTheme.secondary,
-              ),
-            ],
+          child: FutureBuilder<User>(
+            future: user,
+            builder: (BuildContext context, AsyncSnapshot<User> snapshot) {
+              if (snapshot.hasData) {
+                return Column(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    const SizedBox(height: 40),
+                    Image.network(
+                      snapshot.data?.photo ?? '',
+                      width: 100,
+                      height: 100,
+                      errorBuilder: (BuildContext context, Object exception, StackTrace? stackTrace) {
+                        return const Icon(
+                          Icons.account_circle,
+                          size: 100,
+                        );
+                      },
+                    ),
+                    const SizedBox(height: 20),
+                    Text(
+                      snapshot.data?.name ?? '',
+                      style: const TextStyle(
+                        fontSize: 24,
+                      ),
+                    ),
+                    const SizedBox(height: 10),
+                    Text(
+                      snapshot.data?.email ?? '',
+                      style: const TextStyle(
+                        color: Colors.grey,
+                        fontSize: 16,
+                      ),
+                    ),
+                    const SizedBox(height: 20),
+                    ButtonInput(
+                      onTap: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(builder: (context) => const Settings()),
+                        );
+                      },
+                      icon: Icons.settings,
+                      message: 'Settings',
+                      theme: ButtonInputTheme.primary,
+                    ),
+                    const SizedBox(height: 20),
+                    ButtonInput(
+                      onTap: signOut,
+                      icon: Icons.arrow_back,
+                      message: 'Sign Out',
+                      theme: ButtonInputTheme.secondary,
+                    ),
+                  ],
+                );
+              } else if (snapshot.hasError) {
+                print('Error: ${snapshot.error}');
+                // signOut();
+              } else {
+                return const CircularProgressIndicator();
+              }
+              return Container();
+            },
           ),
         ),
       ),
