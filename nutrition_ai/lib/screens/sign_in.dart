1045769df94ab197165c110528c65d3bcd11450b
ModupeD <69601432+ModupeD@users.noreply.github.com>
2023-11-06 16:00:57 -0500

    Redesigned login (#73)
    * Redesifned sign in page
    
    * Reformat code to match "dart format"
    
    * Shorten FadeInUp Animation Duration
    
    ---------
    
    Co-authored-by: Nathan Wick <nathanwick2001@gmail.com>

--- nutrition_ai/lib/screens/sign_in.dart
+++ nutrition_ai/lib/screens/sign_in.dart
@@ -1,10 +1,9 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:animate_do/animate_do.dart';
 
 import '../services/authentication.dart';
-import '../widgets/button_input.dart';
 import '../widgets/square_tile.dart';
-import '../widgets/text_input.dart';
 
 class SignInScreen extends StatefulWidget {
   const SignInScreen({super.key});
@@ -27,95 +26,206 @@ class _SignInScreenState extends State<SignInScreen> {
       },
     );
     try {
-      await FirebaseAuth.instance.signInWithEmailAndPassword(
-        email: emailController.text,
-        password: passwordController.text,
-      ).then((value) => Navigator.pop(context));
+      await FirebaseAuth.instance
+          .signInWithEmailAndPassword(
+            email: emailController.text,
+            password: passwordController.text,
+          )
+          .then((value) => Navigator.pop(context));
     } on FirebaseAuthException catch (error) {
       Navigator.pop(context);
       showDialog(
           context: context,
           builder: (context) {
-            return AlertDialog(
-                title: Text('${error.code}: ${error.message}')
-            );
-          }
-      );
+            return AlertDialog(title: Text('${error.code}: ${error.message}'));
+          });
     }
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SafeArea(
-        child: SingleChildScrollView(
+      body: SingleChildScrollView(
+        child: Container(
+          width: double.infinity,
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              begin: Alignment.topCenter,
+              colors: [
+                Color.fromARGB(255, 235, 255, 202),
+                Color.fromARGB(255, 235, 255, 202),
+                Color.fromARGB(10, 255, 150, 300),
+              ],
+            ),
+          ),
           child: Column(
-            mainAxisAlignment: MainAxisAlignment.start,
-            children: [
-              const SizedBox(height: 80),
-              const Icon(
-                Icons.lock,
-                size: 100,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              const SizedBox(
+                height: 80,
               ),
-              const SizedBox(height: 40),
-              TextInput(
-                controller: emailController,
-                name: 'Email',
-              ),
-              const SizedBox(height: 10),
-              TextInput(
-                controller: passwordController,
-                name: 'Password',
-                obscureText: true,
-              ),
-              const SizedBox(height: 10),
-              ButtonInput(
-                onTap: signIn,
-                icon: Icons.arrow_forward,
-                message: 'Sign In',
-                theme: ButtonInputTheme.primary,
-              ),
-              const SizedBox(height: 80),
               Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 20),
-                child: Row(
-                  children: const [
-                    Expanded(
-                      child: Divider(
-                        thickness: 0.4,
-                        color: Colors.grey,
+                padding: const EdgeInsets.all(20),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    FadeInUp(
+                      duration: const Duration(milliseconds: 400),
+                      child: const Text(
+                        "Login",
+                        style: TextStyle(
+                            color: Color.fromARGB(255, 28, 77, 0),
+                            fontSize: 50,
+                            fontWeight: FontWeight.w600),
                       ),
                     ),
-                    Padding(
-                      padding: EdgeInsets.symmetric(horizontal: 10),
-                      child: Text(
-                        'Or Continue With',
-                        style: TextStyle(color: Colors.black),
-                      ),
+                    const SizedBox(
+                      height: 5,
                     ),
-                    Expanded(
-                      child: Divider(
-                        thickness: 0.4,
-                        color: Colors.grey,
+                    FadeInUp(
+                      duration: const Duration(milliseconds: 800),
+                      child: const Text(
+                        "Welcome Back",
+                        style: TextStyle(
+                            color: Color.fromARGB(255, 2, 123, 0),
+                            fontSize: 18),
                       ),
                     ),
                   ],
                 ),
               ),
-              const SizedBox(height: 40),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  SquareTile(
-                    onTap: () => AuthenticationService().signInWithGoogle(),
-                    imagePath: 'assets/images/logos/google.png',
+              const SizedBox(height: 50),
+              Container(
+                decoration: const BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.only(
+                        topLeft: Radius.circular(60),
+                        topRight: Radius.circular(60))),
+                child: Padding(
+                  padding: const EdgeInsets.all(30),
+                  child: Column(
+                    children: <Widget>[
+                      const SizedBox(
+                        height: 10,
+                      ),
+                      FadeInUp(
+                          duration: const Duration(milliseconds: 1000),
+                          child: Container(
+                            decoration: BoxDecoration(
+                                color: Colors.white,
+                                borderRadius: BorderRadius.circular(10),
+                                boxShadow: const [
+                                  BoxShadow(
+                                      color: Color.fromRGBO(13, 131, 78, 0.298),
+                                      blurRadius: 20,
+                                      offset: Offset(0, 10))
+                                ]),
+                            child: SingleChildScrollView(
+                              child: Column(
+                                children: <Widget>[
+                                  Container(
+                                    padding: const EdgeInsets.all(10),
+                                    decoration: BoxDecoration(
+                                        border: Border(
+                                            bottom: BorderSide(
+                                                color: Colors.grey.shade200))),
+                                    child: TextField(
+                                      controller: emailController,
+                                      decoration: const InputDecoration(
+                                          hintText: "Email ",
+                                          hintStyle:
+                                              TextStyle(color: Colors.grey),
+                                          border: InputBorder.none),
+                                    ),
+                                  ),
+                                  Container(
+                                    padding: const EdgeInsets.all(10),
+                                    decoration: BoxDecoration(
+                                        border: Border(
+                                            bottom: BorderSide(
+                                                color: Colors.grey.shade200))),
+                                    child: TextField(
+                                      controller: passwordController,
+                                      obscureText: true,
+                                      decoration: const InputDecoration(
+                                          hintText: "Password",
+                                          hintStyle:
+                                              TextStyle(color: Colors.grey),
+                                          border: InputBorder.none),
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ),
+                          )),
+                      const SizedBox(
+                        height: 30,
+                      ),
+                      FadeInUp(
+                          duration: const Duration(milliseconds: 1000),
+                          child: const Text(
+                            "Forgot Password?",
+                            style: TextStyle(color: Colors.grey),
+                          )),
+                      const SizedBox(
+                        height: 30,
+                      ),
+                      FadeInUp(
+                          duration: const Duration(milliseconds: 1000),
+                          child: MaterialButton(
+                            onPressed: signIn,
+                            height: 50,
+                            color: const Color.fromARGB(255, 28, 77, 0),
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(50),
+                            ),
+                            child: const Center(
+                              child: Text(
+                                "Login",
+                                style: TextStyle(
+                                    color: Colors.white,
+                                    fontWeight: FontWeight.bold),
+                              ),
+                            ),
+                          )),
+                      const SizedBox(
+                        height: 50,
+                      ),
+                      FadeInUp(
+                          duration: const Duration(milliseconds: 1000),
+                          child: const Text(
+                            "Sign in with ",
+                            style: TextStyle(color: Colors.grey),
+                          )),
+                      const SizedBox(
+                        height: 15,
+                      ),
+                      Center(
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: <Widget>[
+                            const SizedBox(height: 40),
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.end,
+                              children: [
+                                SquareTile(
+                                  onTap: () => AuthenticationService()
+                                      .signInWithGoogle(),
+                                  imagePath: 'assets/images/logos/google.png',
+                                ),
+                                const SizedBox(width: 20),
+                                SquareTile(
+                                  onTap: () {},
+                                  imagePath: 'assets/images/logos/apple.png',
+                                ),
+                              ],
+                            ),
+                          ],
+                        ),
+                      ),
+                    ],
                   ),
-                  const SizedBox(width: 20),
-                  SquareTile(
-                    onTap: () {},
-                    imagePath: 'assets/images/logos/apple.png',
-                  )
-                ],
+                ),
               ),
             ],
           ),
