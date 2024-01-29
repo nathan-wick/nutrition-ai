074b41e927bc12319e05982c416b13f1cbee0ac0
Tara <75596401+t820@users.noreply.github.com>
2024-01-28 20:06:38 -0500

    Added types for old data

---
+++ nutrition_ai/functions/src/types/old_data/Ingredient.ts
@@ -0,0 +1,12 @@
+export declare interface Food {
+code: string,
+foodCategoryCode: string,
+foodCategoryName: string,
+foodCode: string,
+foodName: string,
+moistureChange: string,
+name: string,
+retentionCode: string,
+sequence: string,
+weight: string
+}
---
+++ nutrition_ai/functions/src/types/old_data/IngredientsNutrients.ts
@@ -0,0 +1,12 @@
+export declare interface Food {
+derivationCode: string,
+fdcId: string,
+foundationYearAcuired: string,
+ingredientCode: string,
+ingredientName: string,
+nutrientCode: string,
+nutrientName: string,
+nutrientValue: string,
+nutrientValueSource: string,
+srAddModYear: string
+}
