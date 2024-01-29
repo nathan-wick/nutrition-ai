ba6cf282c8ba60c7f35d57ca8ba6b038d846fe39
Nathan Wick <nathanwick2001@gmail.com>
2024-01-28 20:15:01 -0500

    Create New Meal

---
+++ nutrition_ai/functions/src/types/new_data/Meal.ts
@@ -0,0 +1,9 @@
+export declare interface Meal {
+    name: string,
+    photo: string,
+    ingredients: string[],
+    area?: string,
+    category?: string,
+    instructions?: string,
+    youtube?: string,
+}
