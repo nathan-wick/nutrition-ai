edf5db51fdf631066c0eb808a72ea334de19b4db
Nathan Wick <nathanwick2001@gmail.com>
2024-02-09 20:16:00 -0500

    Create Profile Type

---
+++ nutrition_ai/functions/src/types/Profile.ts
@@ -0,0 +1,10 @@
+import {Measurement,} from "./Measurement";
+
+export declare interface Profile {
+    birthday: Date,
+    sex: `male` | `female`,
+    height: Measurement,
+    weight: Measurement,
+    exerciseFrequency: `never` | `sometimes` | `often`,
+    goal: `maintain` | `lose_fat` | `gain_fat` | `gain_muscle`,
+}
--- nutrition_ai/functions/src/types/User.ts
+++ nutrition_ai/functions/src/types/User.ts
@@ -1,16 +1,11 @@
 import {Food,} from "./Food";
-import {Measurement,} from "./Measurement";
+import {Profile,} from "./Profile";
 
 export declare interface User {
     name: string,
     email: string,
     photo: string,
-    birthday: Date,
-    sex: `male` | `female`,
-    height: Measurement,
-    weight: Measurement,
-    exerciseFrequency: `never` | `sometimes` | `often`,
-    goal: `maintain` | `lose_fat` | `gain_fat` | `gain_muscle`,
+    profile: Profile,
     approvedFoods: Food[],
     rejectedFoods: Food[],
 }
