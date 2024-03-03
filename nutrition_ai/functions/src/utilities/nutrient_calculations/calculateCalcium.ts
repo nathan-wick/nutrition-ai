8ef431dc5449372653e2873e240f23986c74523c
Nathan Wick <nathanwick2001@gmail.com>
2024-03-02 23:13:08 -0500

    Update Calcium

--- nutrition_ai/functions/src/utilities/nutrient_calculations/calculateCalcium.ts
+++ nutrition_ai/functions/src/utilities/nutrient_calculations/calculateCalcium.ts
@@ -2,43 +2,54 @@ import {Profile,} from "../../types/Profile";
 
 export const calculateCalcium = (profile: Profile,) => {
 
-    const sexFactor = profile.sex === `female`
-            ? 1.2
-            : 1.0,
-        activityFactor = {
-            "never": 1.0,
-            "often": 1.2,
-            "sometimes": 1.1,
-        }[profile.exerciseFrequency];
-    let calciumRDA = 1000,
-        goalFactor = 1;
-    if (profile.age && profile.age <= 50) {
+    const age = profile.age ?? 22;
+    let amount = 0;
+    if (age < 19) {
 
-        calciumRDA = 1000;
+        amount += 300;
 
-    } else if (profile.age && profile.age <= 70) {
+    } else if (age < 51) {
 
-        calciumRDA = 1200;
+        amount += 1000;
+
+    } else if (age < 71) {
+
+        if (profile.sex === `male`) {
+
+            amount += 1000;
+
+        } else {
+
+            amount += 1200;
+
+        }
 
     } else {
 
-        calciumRDA = 1300;
+        amount += 1200;
 
     }
-    switch (profile.goal) {
+    if (profile.exerciseFrequency === `often`) {
 
-    case `gain_muscle`:
-        goalFactor = 1.2;
-        break;
-    case `lose_fat`:
-        goalFactor = 1.1;
-        break;
-    default:
-        goalFactor = 1;
+        amount += 200;
+
+    } else if (profile.exerciseFrequency === `sometimes`) {
+
+        amount += 100;
+
+    }
+    if (profile.bodyMassIndex ?? 0 > 30) {
+
+        amount += 100;
+
+    }
+    if (profile.goal === `gain_muscle`) {
+
+        amount += 200;
 
     }
     return {
-        "amount": calciumRDA * sexFactor * goalFactor * activityFactor,
+        amount,
         "unit": `mg`,
     };
 
