05d8015a2af65b73444d33c47cc37f529429cd6c
ModupeD <daniemu@mail.uc.edu>
2024-03-07 10:13:50 -0500

    Fixed image distortion issue

--- nutrition_ai/lib/screens/authenticated/preferences.dart
+++ nutrition_ai/lib/screens/authenticated/preferences.dart
@@ -126,95 +126,195 @@ class _PreferencesScreenState extends State<PreferencesScreen> {
     if (foods.isEmpty) {
       fetchFoods(user);
     }
-return Scaffold(
-  body: food != null
-      ? Container(
-          color: const Color.fromARGB(255, 176, 227, 178),
-          height: 400,
-          child: Stack(
-            children: [
-              Column(
+    return Scaffold(
+      body: food != null
+          ? Container(
+              decoration: BoxDecoration(
+                color: Color.fromARGB(149, 133, 147, 134),
+                image: DecorationImage(
+                  image: NetworkImage(
+                      "https://res.cloudinary.com/drf6yjgkn/image/upload/v1709818526/by93hhaa368ftsshtaxy.jpg"), // Adjust the image path
+                  fit: BoxFit
+                      .fitHeight, // This will cover the entire container space
+                ),
+              ),
+              // Use the full screen height instead of a fixed height to accommodate content dynamically
+              height: MediaQuery.of(context).size.height,
+              child: Stack(
                 children: [
-                  Expanded(
-                    flex: 1,
+                  // Positioned container for the rounded corner effect and background color change
+                  Positioned(
+                    top:
+                        250, // Adjust this value based on the image size and your desired spacing
+                    left: 0,
+                    right: 0,
+                    bottom: 0, // Extends to the bottom of the screen
                     child: Container(
-                      child: const SizedBox(height: 20),
-                    ),
-                  ),
-                  Expanded(
-                    flex: 1,
-                    child: Container(
-                      decoration: BoxDecoration(
+                      decoration: const BoxDecoration(
                         borderRadius: BorderRadius.only(
                           topLeft: Radius.circular(50),
                           topRight: Radius.circular(50),
                         ),
                         color: Color.fromARGB(255, 243, 252, 243),
                       ),
+                      child: SingleChildScrollView(
+  child: Column(
+    crossAxisAlignment: CrossAxisAlignment.start,
+    children: [
+      Padding(
+        padding: const EdgeInsets.only(top: 16.0,bottom:100, left: 16.0, right:16.0, ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            const SizedBox(height: 100),
+            Container(
+              width: MediaQuery.of(context).size.width, // Set the width to the full width of the screen
+              height: 400, // Set a fixed height for the container
+              child: Card(
+                // Title and Ingredients list here
+                child: Padding(
+                  padding: const EdgeInsets.all(16.0),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        food.name,
+                        style: const TextStyle(
+                          fontSize: 24,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      const SizedBox(height: 20),
+                      const Text(
+                        "Ingredients",
+                        style: TextStyle(
+                          fontSize: 20,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      const SizedBox(height: 8.0),
+                      // List your ingredients here
+                      Expanded( // Wrap the Column with Expanded
+                        child: SingleChildScrollView( // Add SingleChildScrollView
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              if (food.ingredients != null)
+                                for (var ingredient in food.ingredients!)
+                                  Text(
+                                    "• ${ingredient.name} - ${ingredient.amount.amount.toStringAsFixed(0)}${ingredient.amount.unit}",
+                                  ),
+                            ],
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    ],
+  ),
+),
+
                     ),
                   ),
-                ],
-              ),
-              if (foodImage != null) // Check if foodImage is not null
-                Positioned(
-                  top: 50, // Adjust top position as needed
-                  left: 0,
-                  right: 0,
-                  child: Align(
-                    alignment: Alignment.center,
-                    child: Container(
-                      margin: EdgeInsets.all(15),
-                      width: 250,
-                      height: 250,
-                      decoration: BoxDecoration(
-                        shape: BoxShape.circle,
-                        boxShadow: [
-                          BoxShadow(
-                            color: Color.fromARGB(255, 93, 92, 92)
-                                .withOpacity(0.3),
-                            offset: Offset(-1, 10),
-                            blurRadius: 10,
+                  // Positioned image container
+                  if (foodImage != null)
+                    Positioned(
+                      top: 100,
+                      left: 0,
+                      right: 0,
+                      child: Align(
+                        alignment: Alignment.center,
+                        child: Container(
+                          margin: EdgeInsets.all(15),
+                          width: 250,
+                          height: 250,
+                          decoration: BoxDecoration(
+                            shape: BoxShape.circle,
+                            boxShadow: [
+                              BoxShadow(
+                                color: Color.fromARGB(255, 93, 92, 92)
+                                    .withOpacity(0.3),
+                                offset: Offset(-1, 10),
+                                blurRadius: 10,
+                              ),
+                            ],
                           ),
-                        ],
-                      ),
-                      child: ClipOval(
-                        child: Image.network(
-                          foodImage!,
-                          fit: BoxFit.cover,
+                          child: ClipOval(
+                            child: Image.network(
+                              foodImage!,
+                              fit: BoxFit.cover,
+                            ),
+                          ),
                         ),
                       ),
                     ),
-                  ),
-                ),
-              Positioned(
-                top: 350, // Adjust top position as needed
-                left: 0,
-                right: 0,
-                child: Align(
-                  alignment: Alignment.center,
-                  child: Text(
-                    food.name,
-                    style: const TextStyle(
-                      fontSize: 24,
-                      fontWeight: FontWeight.bold,
+                  Align(
+                    alignment: Alignment.bottomCenter,
+                    child: Padding(
+                      padding: const EdgeInsets.only(
+                          bottom: 15,
+                          left: 10,
+                          right: 10), // Adjust padding values as needed
+                      child: Container(
+                        height: 50, // Fixed height for the button row
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                          children: [
+                            ButtonInput(
+                              onTap: () {
+                                rejectFood(userProvider, user);
+                              },
+                              icon: Icons.thumb_down,
+                              message: 'Reject',
+                              theme: ButtonInputTheme.danger,
+                            ),
+                            const SizedBox(width: 6),
+                            ButtonInput(
+                              onTap: () {
+                                nextFood(user);
+                              },
+                              icon: Icons.arrow_forward_ios,
+                              message: 'Skip',
+                              theme: ButtonInputTheme.secondary,
+                            ),
+                            const SizedBox(width: 6),
+                            Expanded(
+                              child: ButtonInput(
+                                onTap: () {
+                                  approveFood(userProvider, user);
+                                },
+                                icon: Icons.thumb_up,
+                                message: 'Approve',
+                                theme: ButtonInputTheme.primary,
+                              ),
+                            ),
+                            const SizedBox(width: 6),
+                          ],
+                        ),
+                      ),
                     ),
-                  ),
-                ),
+                  )
+                ],
               ),
-            ],
-          ),
-        )
-      : const Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              Text("You've run out of food! Please come again later."),
-            ],
-          ),
-        ),
-  bottomNavigationBar: const MainNavigationBar(
-    defaultIndex: 1,
-  ),
-);
-  }}
+            )
+          : const Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Text("You've run out of food! Please come again later."),
+                ],
+              ),
+            ),
+      bottomNavigationBar: const MainNavigationBar(
+        defaultIndex: 1,
+      ),
+    );
+  }
+}
